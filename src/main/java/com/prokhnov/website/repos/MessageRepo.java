035a48fb7221435118a702a023d8bb6dcce08ac7
WODweqFS <prohnovigor11@gmail.com>
2022-05-09 11:58:19 +0300

    Initial commit

---
+++ src/main/java/com/prokhnov/website/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.prokhnov.website.repos;
+
+import com.prokhnov.website.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
