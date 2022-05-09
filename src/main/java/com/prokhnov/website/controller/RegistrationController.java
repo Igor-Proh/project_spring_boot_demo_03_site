60a2334812b0922c96b74e32f09739f11aa92a86
WODweqFS <prohnovigor11@gmail.com>
2022-05-09 17:40:52 +0300

    Add name to message

--- src/main/java/com/prokhnov/website/controller/RegistrationController.java
+++ src/main/java/com/prokhnov/website/controller/RegistrationController.java
@@ -15,16 +15,17 @@ import java.util.Map;
 public class RegistrationController {
     @Autowired
     private UserRepo userRepo;
+
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model){
+    public String addUser(User user, Map<String, Object> model) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
 
-        if (userFromDb != null){
+        if (userFromDb != null) {
             model.put("message", "User exist!");
             return "registration";
         }
