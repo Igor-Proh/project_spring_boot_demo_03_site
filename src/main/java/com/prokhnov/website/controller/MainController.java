1abfe1f3634f09345fba64fc267c976ee8990f31
WODweqFS <prohnovigor11@gmail.com>
2022-05-10 17:11:04 +0300

    update

--- src/main/java/com/prokhnov/website/controller/MainController.java
+++ src/main/java/com/prokhnov/website/controller/MainController.java
@@ -4,19 +4,26 @@ import com.prokhnov.website.domain.Message;
 import com.prokhnov.website.domain.User;
 import com.prokhnov.website.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepo messageRepo;
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -42,10 +49,22 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model) {
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile file) throws IOException {
         Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+            message.setFilename(resultFileName);
+
+            file.transferTo(new File(uploadPath + "/" + resultFileName));
+        }
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
 
